19cb5062d3faf0b5862bca3a340dc3d9b2492be5
kseniyaaltuhova <bogomolowakseniya@yandex.ru>
2022-03-06 12:20:43 +0300

    add radio project

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,70 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int currentStation;
+    private int currentVolume;
+    private int maxStation = 9;
+    private int minStation = 0;
+    private int maxVolume = 10;
+    private int minVolume = 0;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation > maxStation) {
+            this.currentStation = maxStation;
+            return;
+        }
+        if (currentStation < minStation) {
+            this.currentStation = minStation;
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void increaseCurrentStation() {
+        if (currentStation == maxStation) {
+            currentStation = minStation;
+            return;
+        }
+        currentStation++;
+    }
+
+    public void decreaseCurrentStation() {
+        if (currentStation == minStation) {
+            currentStation = maxStation;
+            return;
+        }
+        currentStation--;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            this.currentVolume = maxVolume;
+            return;
+        }
+        if (currentVolume < minVolume) {
+            this.currentVolume = minVolume;
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void increaseCurrentVolume() {
+        if (currentVolume < maxVolume) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseCurrentVolume() {
+        if (currentVolume > minVolume) {
+            currentVolume--;
+        }
+    }
+}
